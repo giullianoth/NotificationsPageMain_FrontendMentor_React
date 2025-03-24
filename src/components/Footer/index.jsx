8a0c7e7507342a4675db6bc6e4f4db94edae719d
Giulliano <tharsoweb@gmail.com>
2025-03-24 16:17:57 -0300

    footer section

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,10 @@ import styles from "./Footer.module.css"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className={styles.attribution}>
+        Challenge by <a href="https://www.frontendmentor.io/challenges/notifications-page-DqK5QAmKbC" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>.
+        Coded by <a href="https://github.com/giullianoth" target="_blank" rel="noopener noreferrer">Giulliano Guimarães</a>.
+    </footer>
   )
 }
 
